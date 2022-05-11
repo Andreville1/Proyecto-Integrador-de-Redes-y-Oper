fbb01fbe4d6fbdef8f29b8bfc702e92503f6053a
Andreville1 <andrevillegas_09@hotmail.com>
2022-05-11 16:30:56 -0600

    Metodos necesarios para el proyecto

---
+++ src/client.py
@@ -0,0 +1,11 @@
+def log_in():
+    print("HOLA MUNDO")
+
+def casting_forever() :
+    print("HOLA MUNDO")
+
+def stop_listening():
+    print("HOLA MUNDO")
+
+def send_messasge():
+    print("HOLA MUNDO")
---
+++ src/server.py
@@ -0,0 +1,14 @@
+def authenticator() :
+    print("HOLA MUNDO")
+
+def listen_forever() :
+    print("HOLA MUNDO")
+
+def stop_listening() :
+    print("HOLA MUNDO")
+
+def bind_connections() :
+    print("HOLA MUNDO")
+
+def receive_message():
+    print("HOLA MUNDO")
