c3e0fee9ed3f63f6cc7b496a27c212ec65812fa2
Andreville1 <andrevillegas_09@hotmail.com>
2022-05-12 20:55:09 -0600

    Modularizacion del Client.py

--- src/client.py
+++ src/client.py
@@ -2,17 +2,9 @@ import sys
 import socket
 import json
 
-# Inicio del envio de la operacion
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-port = 8080
-seq = 0
-ack = 0
-fin = True
-while True: 
-    bufferSize = 1024
-    serverAddressPort = ("127.0.0.1", port)
-
+def sendOperation() :
     operation = input("Ingrese la operacion: ")
+
     dataJson = {"seq":seq,"type":"request","fin":fin,"request":"write","operation":"operation"}
     jsonString = json.dump(dataJson)
     # Se encripta
@@ -20,8 +12,14 @@ while True:
 
     # Envia la operacion
     UDPClientSocket.sendto(jsonToSend, serverAddressPort)
-    
-    #Recibe la veirificacion de que recibio el producto
+
+def receiveVerification() :
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    verification = msgFromServer[0].decode()
+    serverAddressPort = msgFromServer[1].decode()
+
+    verificationJson = json.loads(verification)
+
     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
     verification = msgFromServer[0].decode()
     serverAddressPort = msgFromServer[1].decode()
@@ -32,36 +30,7 @@ while True:
         if verificationJson["ack"] == ack:
             if verificationJson["seq"] == seq:
 
-                
-                # Recibe la operacion
-                msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-                operation = msgFromServer[0].decode()
-                serverAddressPort = msgFromServer[1].decode()
-
-                operationJson = json.loads(operation)
-                
-                if verificationJson["seq"] == seq:
-                    if verificationJson["type"] == "request":
-                        if verificationJson["fin"] == True:
-                            if verificationJson["request"] == "write":
-                                print(verificationJson["result"])
-
-
-                                dataJson = {"type":"ack","ack":ack,"seq":seq}
-                                jsonString = json.dumb(dataJson)
-                                # Se encripta
-                                jsonToSend = str.encode()
-
-                                # Envia la verificacion que recibio el resultado
-                                UDPClientSocket.sendto(jsonToSend, serverAddressPort)
-                            else:
-                                print("Request erroneo")
-                        else:
-                            print("DEBE ESPERAR LO FRACCIONADO")
-                    else:
-                        print("Type erroneo")
-                else:
-                    print("Seq erroneo")
+                receiveOperation()
 
             else:
                 print("Seq erroneo")
@@ -70,9 +39,51 @@ while True:
     else:
         print("Ack erroneo")
 
+def receiveOperation() :
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    operation = msgFromServer[0].decode()
+    serverAddressPort = msgFromServer[1].decode()
 
+    operationJson = json.loads(operation)
 
+    if operationJson["seq"] == seq:
+        if operationJson["type"] == "request":
+            if operationJson["fin"] == True:
+                if operationJson["request"] == "write":
+                    print(operationJson["result"])
 
+                                
+                    sendVerification()
+                else:
+                    print("Request erroneo")
+            else:
+                print("DEBE ESPERAR LO FRACCIONADO")
+        else:
+            print("Type erroneo")
+    else:
+        print("Seq erroneo")
+
+def sendVerification() :
+    dataJson = {"type":"ack","ack":ack,"seq":seq}
+    jsonString = json.dump(dataJson)
+    # Se encripta
+    jsonToSend = str.encode()
+
+    # Envia la verificacion que recibio el resultado
+    UDPClientSocket.sendto(jsonToSend, serverAddressPort)
+
+# Inicio del envio de la operacion
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+port = 8080
+bufferSize = 1024
+seq = 0
+ack = 0
+fin = True
+serverAddressPort = ("127.0.0.1", port)
+while True: 
+    sendOperation()
+    receiveVerification()
+    
 def encryption() :
     print("HOLA MUNDO")
 
