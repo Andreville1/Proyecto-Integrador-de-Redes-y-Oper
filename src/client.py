6ff0b1c16a08b076f66134379deb800a3c723428
Andreville1 <andrevillegas_09@hotmail.com>
2022-05-12 18:52:17 -0600

    Primer avance del envio y recibimiento de la operacion

--- src/client.py
+++ src/client.py
@@ -1,5 +1,95 @@
-def log_in():
+import sys
+import socket
+import json
+
+# Inicio del envio de la operacion
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+port = 8080
+seq = 0
+ack = 0
+fin = True
+while True: 
+    bufferSize = 1024
+    serverAddressPort = ("127.0.0.1", port)
+
+    operation = input("Ingrese la operacion: ")
+    dataJson = {"seq":seq,"type":"request","fin":fin,"request":"write","operation":"operation"}
+    jsonString = json.dump(dataJson)
+    # Se encripta
+    jsonToSend = str.encode(jsonString)
+
+    # Envia la operacion
+    UDPClientSocket.sendto(jsonToSend, serverAddressPort)
+    
+    #Recibe la veirificacion de que recibio el producto
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    verification = msgFromServer[0].decode()
+    serverAddressPort = msgFromServer[1].decode()
+
+    verificationJson = json.loads(verification)
+
+    if verificationJson["type"] == "ack":
+        if verificationJson["ack"] == ack:
+            if verificationJson["seq"] == seq:
+
+                
+                # Recibe la operacion
+                msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+                operation = msgFromServer[0].decode()
+                serverAddressPort = msgFromServer[1].decode()
+
+                operationJson = json.loads(operation)
+                
+                if verificationJson["seq"] == seq:
+                    if verificationJson["type"] == "request":
+                        if verificationJson["fin"] == True:
+                            if verificationJson["request"] == "write":
+                                print(verificationJson["result"])
+
+
+                                dataJson = {"type":"ack","ack":ack,"seq":seq}
+                                jsonString = json.dumb(dataJson)
+                                # Se encripta
+                                jsonToSend = str.encode()
+
+                                # Envia la verificacion que recibio el resultado
+                                UDPClientSocket.sendto(jsonToSend, serverAddressPort)
+                            else:
+                                print("Request erroneo")
+                        else:
+                            print("DEBE ESPERAR LO FRACCIONADO")
+                    else:
+                        print("Type erroneo")
+                else:
+                    print("Seq erroneo")
+
+            else:
+                print("Seq erroneo")
+        else: 
+            print("Ack erroneo")
+    else:
+        print("Ack erroneo")
+
+
+
+
+def encryption() :
     print("HOLA MUNDO")
+
+def log_in(argv):
+
+    if sys.argv[1] == "-u":
+        username = sys.argv[2]
+        print(username)
+    else:
+        print ("-u incorrecto")
+    
+    if sys.argv[3] == "-p": 
+        password = sys.argv[4]
+        print(password)
+    else:
+        print ("-p incorrecto")
+    
 
 def casting_forever() :
     print("HOLA MUNDO")
--- src/server.py
+++ src/server.py
@@ -1,3 +1,73 @@
+import socket
+import json
+
+# Inicio del recibimiento del producto
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+host = "127.0.0.1"
+port = 8080
+bufferSize = 1024
+
+ack = 0
+seq = 0
+fin = True
+UDPServerSocket.bind((host, port))
+
+while True:
+    # Recibe la operacion
+    jsonToReceive = UDPServerSocket.recvfrom(bufferSize)
+    operation = jsonToReceive[0]
+    serverAddressPort = jsonToReceive[1]
+
+    oper = operation.decode()
+
+    operationJson = json.loads(oper)
+    
+    if operationJson["seq"] == 0: 
+        if operationJson["type"] == "request":
+            if operationJson["fin"] == True:
+                if operationJson["request"] == "write":
+                    operation = ""
+                    print("CONVERTIR LA OPERACION")
+                    result = 0
+
+                    dataJson = {"type":"ack","ack":ack,"seq":seq}
+                    jsonString = json.dump(dataJson)
+                    # Se encripta
+                    jsonToSend = str.encode(jsonString)
+
+                    # Envia verificacion de que recibio el producto
+                    UDPServerSocket.sendto(jsonToSend, serverAddressPort)
+
+
+                    dataJson = {"seq":seq,"type":"request","fin":fin,"request":"write","result":result,"operation":operation}
+                    jsonString = json.dump(dataJson)
+                    # Se encripta
+                    jsonToSend = str.encode(jsonString)
+
+                    # Envia el resultado
+                    UDPServerSocket.sendto(jsonToSend, serverAddressPort)
+
+
+                    # Recibe la verificacion de que le llego el resultado
+                    jsonToReceive = UDPServerSocket.recvfrom(bufferSize)
+                    verification = jsonToReceive[0]
+                    serverAddressPort = jsonToReceive[1]
+
+                    veri = verification.decode()
+
+                    operationJson = json.loads(veri)
+
+
+                else:
+                    print("Request erroneo")
+            else:
+                print("TIENE QUE HACERSE FRACCIONADO")
+        else:
+            print("Type erroneo")
+    else:
+        print("Numero de secuencia erroneo")
+
+
 def authenticator() :
     print("HOLA MUNDO")
 
