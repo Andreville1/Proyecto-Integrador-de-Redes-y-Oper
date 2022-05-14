6fd3f4259ea463c00ec1a1e225a45acf320f2da9
Lucia <lucia.elizondo.sancho@ucr.ac.cr>
2022-05-14 14:40:41 -0600

    Refactor para client en una clase

--- src/client.py
+++ src/client.py
@@ -1,131 +1,127 @@
-import sys
-import socket
+from http import client
 import json
+import random
+from socket import socket
 
-def sendOperation() :
-    operation = input("Ingrese la operacion: ")
-
-    dataJson = {"seq":seq,"type":"request","fin":fin,"request":"write","operation":"operation"}
-    jsonString = json.dump(dataJson)
-    # Se encripta
-    jsonToSend = str.encode(jsonString)
-
-    # Envia la operacion
-    UDPClientSocket.sendto(jsonToSend, serverAddressPort)
-
-def receiveVerification() :
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    verification = msgFromServer[0].decode()
-    serverAddressPort = msgFromServer[1].decode()
-
-    verificationJson = json.loads(verification)
-
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    verification = msgFromServer[0].decode()
-    serverAddressPort = msgFromServer[1].decode()
-
-    verificationJson = json.loads(verification)
-
-    if verificationJson["type"] == "ack":
-        if verificationJson["ack"] == ack:
-            if verificationJson["seq"] == seq:
-
-                receiveOperation()
-
-            else:
-                print("Seq erroneo")
-        else: 
-            print("Ack erroneo")
-    else:
-        print("Ack erroneo")
-
-def receiveOperation() :
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    operation = msgFromServer[0].decode()
-    serverAddressPort = msgFromServer[1].decode()
-
-    operationJson = json.loads(operation)
-
-    if operationJson["seq"] == seq:
-        if operationJson["type"] == "request":
-            if operationJson["fin"] == True:
-                if operationJson["request"] == "write":
-                    print(operationJson["result"])
-
-                                
-                    sendVerification()
-                else:
-                    print("Request erroneo")
-            else:
-                print("DEBE ESPERAR LO FRACCIONADO")
-        else:
-            print("Type erroneo")
-    else:
-        print("Seq erroneo")
-
-def sendVerification() :
-    dataJson = {"type":"ack","ack":ack,"seq":seq}
-    jsonString = json.dump(dataJson)
-    # Se encripta
-    jsonToSend = str.encode()
-
-    # Envia la verificacion que recibio el resultado
-    UDPClientSocket.sendto(jsonToSend, serverAddressPort)
-
-    sendRequest()
-
-def sendRequest() :
-    opcion = input("Ingrese una opcion: ")
-
-    if opcion[0] == "-" and opcion[1] == "q" :
-        dataJson = {"seq":seq,"type":"disconnect"}
-        jsonString = json.dump(dataJson)
-        # Se encripta
-        jsonToSend = str.encode(jsonString)
-
-        # Envia la solicitud de desconexion
-        UDPClientSocket.sendto(jsonToSend, serverAddressPort)
-        UDPClientSocket.close()
-    else :
-        print("HACE OTRA OPERACION")
-
-# Inicio del envio de la operacion
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-port = 8080
-bufferSize = 1024
-seq = 0
-ack = 0
-fin = True
-serverAddressPort = ("127.0.0.1", port)
-
-while True: 
-    sendOperation()
-    receiveVerification()
+from pip import main
 
 
-def encryption() :
-    print("HOLA MUNDO")
+class Client:
+	def __init__(self, address, port):
+		self.address_port = (address, port)
+		self.UDP_socket = socket.socket(
+			family=socket.AF_INET, type=socket.SOCK_DGRAM)
+		self.buffer_size = 128
+		# TODO: put random values for numSeq and numAck
+		self.seq = 0
+		self.ack = 0
+		self.fin = True
 
-def log_in(argv):
+	def send_json(self, data_json):
+		json_string = json.dump(data_json)
+		# Se encripta
+		json_to_send = str.encode(json_string)
 
-    if sys.argv[1] == "-u":
-        username = sys.argv[2]
-        print(username)
-    else:
-        print ("-u incorrecto")
-    
-    if sys.argv[3] == "-p": 
-        password = sys.argv[4]
-        print(password)
-    else:
-        print ("-p incorrecto")
-    
+		# Envia la solicitud de desconexion
+		self.UDP_socket.sendto(json_to_send, self.address_port)
 
-def casting_forever() :
-    print("HOLA MUNDO")
+	def option_quit(self):
+		data_json = {"seq": self.seq, "type": "disconnect"}
+		self.send_json(data_json)
+		self.UDP_socket.close()
 
-def stop_listening():
-    print("HOLA MUNDO")
+	def send_request(self):
+		opcion = input("Ingrese una opcion: ")
 
-def send_messasge():
-    print("HOLA MUNDO")
+		if opcion[0] == "-" and opcion[1] == "q":
+			self.option_quit()
+		else:
+			print("HACE OTRA OPERACION")
+
+	def send_verification(self):
+		data_json = {"type": "ack", "ack": self.ack, "seq": self.seq}
+		self.send_json(data_json)
+
+		self.send_request()
+
+	def send_operation(self):
+		operation = input("Ingrese la operacion: ")
+		data_json = {"seq": self.seq, "type": "request",
+                    "fin": self.fin, "request": "write", "operation": operation}
+		self.send_json(data_json)
+
+	def recieve_operation(self):
+		#TODO: Refactor this:
+		msg_from_server = self.UDP_socket.recvfrom(self.buffer_size)
+		operation = msg_from_server[0].decode()
+		serverAddressPort = msg_from_server[1].decode()
+
+		operation_json = json.loads(operation)
+
+		if operation_json["seq"] == self.seq:
+			if operation_json["type"] == "request":
+				if operation_json["fin"] == True:
+					if operation_json["request"] == "write":
+						print(operation_json["result"])
+						self.send_verification()
+					else:
+						print("Request erroneo")
+				else:
+					print("DEBE ESPERAR LO FRACCIONADO")
+			else:
+				print("Type erroneo")
+		else:
+			print("Seq erroneo")
+
+	def receive_operation(self):
+		#TODO: refactor this:
+		msg_from_server = self.UDP_socket.recvfrom(self.buffer_size)
+		operation = msg_from_server[0].decode()
+		serverAddressPort = msg_from_server[1].decode()
+
+		operation_json = json.loads(operation)
+
+		if operation_json["seq"] == self.seq:
+			if operation_json["type"] == "request":
+				if operation_json["fin"] == True:
+					if operation_json["request"] == "write":
+						print(operation_json["result"])
+						self.send_verification()
+
+					else:
+						print("Request erroneo")
+				else:
+					print("DEBE ESPERAR LO FRACCIONADO")
+			else:
+				print("Type erroneo")
+		else:
+			print("Seq erroneo")
+
+	def receive_verification(self):
+		#TODO: refactor this:
+		msg_from_server = self.UDP_socket.recvfrom(self.buffer_size)
+		verification = msg_from_server[0].decode()
+		serverAddressPort = msg_from_server[1].decode()
+		verification_json = json.loads(verification)
+
+		if verification_json["type"] == "ack":
+			if verification_json["ack"] == self.ack:
+				if verification_json["seq"] == self.seq:
+					self.receive_operation()
+
+				else:
+					print("Seq erroneo")
+			else:
+				print("Ack erroneo")
+		else:
+			print("Ack erroneo")
+
+	def main(self):
+		while(True):
+			self.send_operation()
+			self.receive_verification()
+
+
+if __name__ == "__main__":
+	client = Client("127.0.0.1", 8080)
+	client.main()
