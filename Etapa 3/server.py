6f026121c2e32db2abaf1429669f9497cc864631
Andreville1 <andrevillegas_09@hotmail.com>
2022-06-15 23:06:02 -0600

    Agregado que el servidor escuche y tambien conecte con otros nodos

--- Etapa 3/server.py
+++ Etapa 3/server.py
@@ -2,6 +2,8 @@ import json
 import random
 import socket
 import threading
+import sys
+from xmlrpc.client import TRANSPORT_ERROR
 
 class Server(object):
 	def __init__(self, address, port):
@@ -24,7 +26,11 @@ class Server(object):
 		self.distanceNeigh = {}
 		self.node = ""
 
+		self.can_continue = False
+		self.neigh_ip = {}
+		self.neigh_port = {}
 
+		self.can_listen = True
 
 	def recv_verification(self, connection):
 		bytes_recv = connection.recv(self.buffer_size)
@@ -42,9 +48,9 @@ class Server(object):
 		# Envia el resultado
 		connection.sendall(bytesToSend)
 
-	def Bellman_Ford(self, source):
+	def Bellman_Ford(self):
 		dist = [float("Inf")] * self.lines
-		dist[source] = 0
+		dist[ord(self.source)-65] = 0
 
 		for _ in range(self.lines - 1):
 			for u, v, w in self.graph:
@@ -66,7 +72,7 @@ class Server(object):
 			else:
 				self.distanceNeigh[self.node] = dist[index]
 		
-		#print(self.distanceNeigh)
+		print(self.distanceNeigh)
 
 	def calc_operation(self, operation_json, address, connection):
 		operation = operation_json["operation"]
@@ -74,8 +80,7 @@ class Server(object):
 		result = eval(operation)
 
 		# LO NUEVO COMIENZA ACA
-		data_json = {"type": "vector", "node": chr(self.source+65), "conn": self.distanceNeigh}
-		print(data_json)
+		#print(data_json)
 		# LO NUEVO TERMINA ACA
 
 		# Envia el resultado
@@ -92,9 +97,11 @@ class Server(object):
 		msg = bytes_recv.decode()
 		json_msg = json.loads(msg)
 
-		if json_msg["type"] == "disconnect":
+		if json_msg["type"] == "disconnect": # Caso en que el cliente se quiere desconectar
 			connection.close()
-		else:
+		elif json_msg["type"] == "vector": # Caso en que le llega un paquete de un nodo
+			print(json_msg)
+		else: # Caso en que recibe un paquete del cliente
 			self.calc_operation(json_msg, address, connection)
 
 	def recv_json(self, connection):
@@ -106,14 +113,46 @@ class Server(object):
 		json_msg = json.loads(msg) # operation_json = json.loads(oper)
 		return json_msg, address
 
-	def listen(self):
-		self.socket_TCP.listen(7)
-		while True:
-			connection, client_address = self.socket_TCP.accept()
-			connection.settimeout(60)
-			threading.Thread(target = self.recv_request, args = (connection,client_address)).start()
+	def send_json(self, data_json, socket_TCP_connect):
+		bytes_to_send = str.encode(json.dumps(data_json))
+		socket_TCP_connect.sendall(bytes_to_send)
 
+	def listen(self):
+		cont = 0
+		while True:
+			if (self.can_listen == True): # Se pone a esuchar por conexiones
+				self.socket_TCP.settimeout(5)
+				try:
+					print("Escuchando")
+					self.socket_TCP.listen(7)
+					connection, client_address = self.socket_TCP.accept()
+					threading.Thread(target = self.recv_request, args = (connection,client_address)).start()
+				except: 
+					self.can_listen = False
+			else: # Se pone a conectar con un nodo vecino
+				socket_TCP_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				socket_TCP_connect.settimeout(2)
+				try:
+					print("Conectando")
+					new_address = (self.neigh_ip[cont], self.neigh_port[cont])
+					
+					socket_TCP_connect.connect(new_address)
+					cont = cont + 1
+					if (cont == len(self.neigh_ip)):
+						cont = 0
+						self.can_listen = True
+					data_json = {"type": "vector", "node": chr(self.source+65), "conn": self.distanceNeigh}
+					self.send_json(data_json, socket_TCP_connect)
+				except:
+					cont = cont + 1
+					if (cont == len(self.neigh_ip)):
+						cont = 0
+						self.can_listen = True
+				
+		
 	def calc_table(self):
+		self.source = sys.argv[1]
+		cont = 0
 		file = open('topologia.csv', 'r')
 		for line in file:
 			self.lines = self.lines + 1
@@ -125,20 +164,26 @@ class Server(object):
 			# Convierte la linea en un arreglo
 			list = line.split(',')
 
-			if (self.lines == 1):
-				self.source = ord(list[0])-65
-
 			#print(lines)
 			#print(list[0] + "/" + list[1] + "/" + list[2] + "/" + list[3] + "/" + list[4]
 			#+ "/" + list[5] + "/" + list[6])
 
 			# Agregue aristas
 			self.graph.append([ord(list[0])-65, ord(list[3])-65, int(list[6])])
+
+			#print(self.source)
+
+			# Guarda la IP y puertos de sus vecinos
+			if (list[0] == self.source): 
+				self.neigh_ip[cont] = str(list[4])
+				self.neigh_port[cont] = int(list[5])
+				cont = cont + 1
 		
 		#print(self.graph)
 		#print(self.source)
 
-		self.Bellman_Ford(self.source)
+		self.Bellman_Ford()
+		self.can_continue = True
 
 def main():
 	server = Server("127.0.0.1", 8080)
