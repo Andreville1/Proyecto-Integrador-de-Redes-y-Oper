0cac5254ec5c78d4861d70a30f9b60d4dc4a3d87
Adrian Madrigal <adrian.madrigalazofeifa@ucr.ac.cr>
2022-06-22 16:59:10 -0600

    Added implementation for second chance / clock algorithm

---
+++ Etapa 3/Oper/PageReplacement.cpp
@@ -0,0 +1,43 @@
+#include "PageReplacement.hpp"
+#include <algorithm>
+
+bool PageReplacement::pagePresent(int page) {
+    return std::find(pages.begin(), pages.end(), page) != pages.end();
+}
+
+void PageReplacement::giveSecondChance(int page) {
+    int index = std::find(pages.begin(), pages.end(), page) - pages.begin();
+    secondChances[index] = true;
+}
+
+void PageReplacement::replacePage(int page) {
+    while (true) {
+        if (!secondChances[pointer]) {
+            pages[pointer] = page;
+            pointer = ((pointer + 1) % num_frames + num_frames)
+                % num_frames;
+            return;
+        }
+
+        secondChances[pointer] = false;
+        pointer = ((pointer + 1) % num_frames + num_frames) % num_frames;
+    }
+}
+
+PageReplacement::PageReplacement() {
+    pages.resize(num_frames);
+    secondChances.resize(num_frames);
+
+    std::fill(pages.begin(), pages.end(), -1);
+    std::fill(secondChances.begin(), secondChances.end(), false);
+}
+
+int PageReplacement::calculateFrame(int page) {
+    if (pagePresent(page)) {
+        giveSecondChance(page);
+        return -1;
+    }
+
+    replacePage(page);
+    return ((pointer - 1) % num_frames + num_frames) % num_frames;
+}
